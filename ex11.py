1f428599ad287791d40c4bbe7b461f53b67ef6ac
rubenvansmirren <ruben.van.smirren@bynder.com>
2017-07-05 10:02:07 +0000

    Adding another few excercises

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("How old are you?"),
+age = input()
+print("How tall are you?"),
+height = input()
+print("How fat are you?"),
+fat = input()
+
+print("So you're {} old, {} tall and {} heavy.".format(age, height, fat)) 
---
+++ ex12.py
@@ -0,0 +1,7 @@
+age = input("How old are you?")
+height = input("how tall are you?")
+weight = input("How much do you weigh?")
+
+print("So you're {} old, {} inches tall and your weight is {} Kg.".format
+    (age, height, weight))
+
