f412e625b1d438a45f96114a4a779bf474317315
rubenvansmirren <ruben.van.smirren@bynder.com>
2017-07-05 08:34:16 +0000

    ex9

---
+++ ex9.py
@@ -0,0 +1,18 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days:", days)
+print("Here are the months:", months)
+
+
+print("""\n\nI can use
+all
+the
+lines
+I
+want
+by
+using
+tripple
+quotes
+""")
